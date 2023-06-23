4efc004361fd0e79038aa2e8546be67d2eb2421a
khanhbinh87 <khanhbinh1187@gmail.com>
2023-06-23 09:09:36 +0700

    crud users

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable()//provider
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello';
   }
 }
