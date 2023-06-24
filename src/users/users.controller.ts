6c6b25597da17204c7a434103a2be5739fe2b08f
khanhbinh87 <khanhbinh1187@gmail.com>
2023-06-24 11:07:31 +0700

    get user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -21,9 +21,9 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
-
+  
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,33 +3,33 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private catModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private useModel: Model<User>) {}
   getHassPassword(password: string) {
     const salt = genSaltSync(10);
     return hashSync(password, salt);
-
   }
   async create(createUserDto: CreateUserDto) {
     let hassPassword = this.getHassPassword(createUserDto.password);
-    let user = await this.catModel.create({
+    let user = await this.useModel.create({
       email: createUserDto.email,
       password: hassPassword,
-      name:createUserDto.name
+      name: createUserDto.name,
     });
-    return createUserDto;
+    return user;
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return { msg: `Not found user` };
+    return await this.useModel.findOne({ _id: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
