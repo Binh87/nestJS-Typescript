437baeb2beb566bd9053e438e8c5312d20c8ddc3
khanhbinh87 <khanhbinh1187@gmail.com>
2023-07-11 22:35:32 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,12 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+
+  });
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
